7bba295659d28ef093cdf28abff6d12e31178bd1
Fatma Al-Shehhi <f_al-shehhi@hotmail.com>
2019-10-25 18:51:27 +0400

    Still on process website

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 6000); // Change image every 2 seconds
+}// Animate Smooth Scroll
+$('#view-work').on('click', function() {
+	const images = $('#images').position().top;
+
+
+	$('html, body').animate({
+		scrollTop: images
+	},
+	900
+	);
+});
